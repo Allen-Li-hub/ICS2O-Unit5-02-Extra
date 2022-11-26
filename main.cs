93c41a430936167a807bf50a9a05a6932151e910
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-11-25 23:04:42 -0500

    change

--- main.cs
+++ main.cs
@@ -1,7 +1,5 @@
 // Created by: Allen Li
-// Created on: Oct 2022
-//
-// This program displays, "Hello, World!"
+// Created on: Nov 2022
 
 using System;
 
@@ -9,7 +7,31 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // This function accepts user input
+        int userNumber;
+        int randomNumber;
+
+        Console.WriteLine("Do you want a positive or negative number?");
+        Console.WriteLine("");
+
+        Console.WriteLine("Enter '1' if positive or '2' if negative ");
+        Console.WriteLine("");
+        userNumber = Convert.ToInt16(Console.ReadLine());
+
+        Random rnd = new Random();
+
+        Console.WriteLine(" ");
+        if (userNumber == 1)
+        {
+            randomNumber = rnd.Next(1, 6);
+            Console.WriteLine("The random number is: " + randomNumber);
+        }
+        else
+        {
+            randomNumber = rnd.Next(-6, -1);
+            Console.WriteLine("The random number is: " + randomNumber);
+        }
+
         Console.WriteLine("\nDone.");
     }
 }
